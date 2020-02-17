07c33a342b1a328d4134c61f0df3ad06ea7ab151
xMAGWAJx <xmagwajx@gmail.com>
2020-02-17 16:48:12 +0200

    Resolved Product Issue.

--- src/lesson3/product/Product.java
+++ src/lesson3/product/Product.java
@@ -5,35 +5,34 @@ public class Product {
     private double regularPrice;
     private double discount;
 
-    public void actualPrice() {
+    public Product (String name) {
+        this.name = name;
+    }
 
+    public double actualPrice() {
+        double discountAmount = 100 - discount;
+        double result = (regularPrice * discountAmount) / 100;
+        return result;
     }
 
     public void printInformation() {
-
+        System.out.println("Product : name = \"" + name + "\", regular price = " + getRegularPrice() + " EUR, discount = " + getDiscount() + "%, actual price = " + actualPrice());
     }
 
-    public String getName() {
-        return name;
-    }
 
     public double getRegularPrice() {
         return regularPrice;
     }
 
-    public double getDiscount() {
-        return discount;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public int getDiscount() {
+        return (int)discount;
     }
 
     public void setRegularPrice(double regularPrice) {
         this.regularPrice = regularPrice;
     }
 
-    public void setDiscount(double discount) {
+    public void setDiscount(int discount) {
         this.discount = discount;
     }
 }
--- src/lesson3/product/ProductDemo.java
+++ src/lesson3/product/ProductDemo.java
@@ -3,5 +3,8 @@ package lesson3.product;
 public class ProductDemo {
     public static void main(String[] args) {
         Product product = new Product("Milk");
+        product.setRegularPrice(0.8);
+        product.setDiscount(20);
+        product.printInformation();
     }
 }
