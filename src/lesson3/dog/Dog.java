a73290b2ab0a0c51dcb46000816e997039029032
Ruslans <xmagwajx@gmail.com>
2020-02-18 23:59:54 +0200

    Lesson 3: Dog - removed extra properties and methods according to task requirement.

--- src/lesson3/dog/Dog.java
+++ src/lesson3/dog/Dog.java
@@ -4,9 +4,6 @@ public class Dog {
     private int age;
     private String color;
     private String name;
-    private boolean eat;
-    private boolean sleep;
-
 
     public Dog(int age, String color, String name) {
         this.age = age;
@@ -18,63 +15,24 @@ public class Dog {
         System.out.println("Bark Bark");
     }
 
-    public void dogEatsFood() {
+    public void eat() {
         System.out.println("chewing a bone");
-        eat = true;
     }
 
-    public void dogDoesNotEatsFood() {
-        System.out.println("Is not hungry");
-        eat = false;
-    }
-
-    public void dogSleeps() {
+    public void sleep() {
         System.out.println("Z-Z-Z-Z-Z-Z-Z-Z-Z-Z-Z-Z-Z");
-        sleep = true;
-    }
-
-    public void dogDoesNotSleep() {
-        System.out.println("Z-Z-Z-Z-Z-Z-Z-Z-Z-Z-Z-Z-Z");
-        sleep = false;
-    }
-
-    public void setSleep(boolean sleep) {
-        this.sleep = sleep;
-    }
-
-    public boolean isSleep() {
-        return sleep;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge (int age) {
-        this.age = age;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getColor() {
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public boolean isEat() {
-        return eat;
-    }
-
-    public void setEat(boolean eat) {
-        this.eat = eat;
-    }
 }
