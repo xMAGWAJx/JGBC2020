36b416a3150912d77df2e4c3ac9bb31c0d4ac8e2
xMAGWAJx <xmagwajx@gmail.com>
2020-02-19 21:25:43 +0200

    Lesson 4: userloginservice - work on this.

---
+++ src/lesson4/userloginservice/User.java
@@ -0,0 +1,20 @@
+package lesson4.userloginservice;
+
+public class User {
+
+  private String login;
+  private String password;
+  private boolean userIsBloocked;
+  private int numberOfAttempts;
+
+
+
+
+  public void resetnumberOfAttempts() {
+
+  }
+
+  public void blockUser() {
+
+  }
+}
---
+++ src/lesson4/userloginservice/UserLoginServiceTest.java
@@ -0,0 +1,4 @@
+package lesson4.userloginservice;
+
+public class UserLoginServiceTest {
+}
