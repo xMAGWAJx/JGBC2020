3bf4c5a15e59967b7ddceb08186646bc2b77e65b
xMAGWAJx <xmagwajx@gmail.com>
2020-02-20 16:35:55 +0200

    Lesson 4: userloginservice task - in progress

--- src/lesson4/userloginservice/UserLoginService.java
+++ src/lesson4/userloginservice/UserLoginService.java
@@ -1,4 +1,13 @@
 package lesson4.userloginservice;
 
 public class UserLoginService {
+
+  public static void main(String[] args) {
+
+    UserLoginService service = new UserLoginService();
+    User userOne = new User("user-login", "user-password");
+​
+    service.login(userOne, "user-password-12345"); //Should return false
+    service.login(userOne, "user-password"); //Should return true
+  }
 }
