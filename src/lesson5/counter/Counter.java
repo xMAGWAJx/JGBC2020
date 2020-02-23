95ff60ef3feed073613d52d5be80aa85539d207c
xMAGWAJx <xmagwajx@gmail.com>
2020-02-23 13:21:05 +0200

    Lesson 5: Counter done.

--- src/lesson5/counter/Counter.java
+++ src/lesson5/counter/Counter.java
@@ -1,90 +1,53 @@
-
-
 package lesson5.counter;
+
 public class Counter {
-
-/*
-
-Методы класса:
-
-increment() - увеличить значение счетчика с учетом шага;
-
-Условие: Если результат сложения текущего состояния счетчика и шага больше 100, тогда операцию не выполнять;
-decrement() - уменьшить значение счетчика с учетом шага;
-
-Условие: Если результат вычитания текущего состояния счетчика от шага меньше 0, тогда операцию не выполнять;
-reset() - сбросить значение и шаг счетчика в начальное состояние;
-
-setValue() - переопределение текущего значения;
-
-Условие: Значение должно быть в диапазоне 0 .. 100;
-getValue() - получение текущего значения;
-
-setStep() - переопределение шага;
-
-Условие: Значение должно быть в диапазоне 1 .. 10;
-getStep() - получение текущего шага;
-
-*/
 
     private int value;
     private int step;
 
     public Counter() {
         this.value = 0;
-        this.value = 1;
+        this.step = 1;
     }
 
-    public void increment() {
-        value++;
+  public int getValue() {
+    System.out.println("Value equals to : " + value);
+    return this.value;
+  }
+
+  public int getStep() {
+    System.out.println("Step equals to: " + step);
+    return step;
+  }
+
+  public void setStep(int step) {
+      if (step >= 1 && step <= 10) {
+        this.step = step;
+      }
+  }
+
+  public void setValue(int value) {
+      if (value >= 0 && value <= 100) {
+        this.value = value;
+      }
+  }
+
+  public void increment() {
+      int incrementValue = this.value + this.step;
+      if ((incrementValue >= 0) && (incrementValue < 100) ) {
+        this.value = incrementValue;
+      }
     }
 
     public void decrement() {
-        value--;
+      int incrementValue = this.value - this.step;
+      if (incrementValue >= 0) {
+        this.value = incrementValue;
+      }
     }
 
     public void clear() {
-        //TODO
+      this.value = 0;
+      this.step = 1;
     }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setStep(int step) {
-        this.step = step;
-    }
-
-    public int getStep() {
-        return step;
-    }
-
-    public static void main(String[] args) {
-
-        Counter counter = new Counter();
-
-        counter.getValue(); //Equals to 0
-        counter.getStep();  //Equals to 1
-
-        counter.increment();
-
-        counter.getValue(); //Equals to 1
-
-        counter.setStep(5);
-
-        counter.increment();
-
-        counter.setStep(2);
-
-        counter.decrement();
-
-        counter.getValue();  //Equals to 4
-        counter.getStep();   //Equals to 2
-
-    }
-
 }
