ceefe6cd55715a5d8447769d8aa720bc42f0d436
Ruslans <xmagwajx@gmail.com>
2020-02-29 14:44:39 +0200

    Lesson 6: NumberService - made changes. Now methods have only one return.

--- src/main/java/lesson6/numberservice/NumberService.java
+++ src/main/java/lesson6/numberservice/NumberService.java
@@ -3,39 +3,35 @@ package lesson6.numberservice;
 public class NumberService {
 
   public int rangeSum(int start, int finish) {
+   int sum = 0;
     if (start < finish) {
-      int sum = 0;
       for (int i = start; i <= finish; i++) {
         sum += i;
       }
-      return sum;
     } else {
-      int sum = 0;
       for (int i = start; i >= finish; i--) {
         sum += i;
       }
-      return sum;
     }
+    return sum;
   }
 
     public int rangeEvenCount(int start, int finish) {
+    int count = 0;
       if (start < finish) {
-        int count = 0;
           for (int i = start; i <= finish; i++) {
              if (i % 2 == 0) {
                count++;
              }
           }
-          return count;
       } else {
-        int count = 0;
         for (int i = start; i >= finish; i--) {
           if (i % 2 == 0) {
             count++;
           }
         }
-        return count;
       }
+      return count;
     }
 }
 
