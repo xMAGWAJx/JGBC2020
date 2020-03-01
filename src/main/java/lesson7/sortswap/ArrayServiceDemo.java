03a88fad8b298f43b347969b024e7c7e23747287
Ruslans <xmagwajx@gmail.com>
2020-03-01 18:11:44 +0200

    Lesson 7: ArrayService (Sort/Swap) - done.

---
+++ src/main/java/lesson7/sortswap/ArrayServiceDemo.java
@@ -0,0 +1,22 @@
+package lesson7.sortswap;
+
+import java.util.Arrays;
+
+public class ArrayServiceDemo {
+
+    public static void main(String[] args) {
+
+        int[] a1 = {4, 7, 2, 3, 8, 9, 5, 1, 6};
+        int[] a2 = {1, 3, 8, 5, 4, 7, 9, 6, 2};
+
+        System.out.println(Arrays.toString(a1)); //[4, 7, 2, 3, 8, 9, 5, 1, 6]
+        System.out.println(Arrays.toString(a2)); //[1, 3, 8, 5, 4, 7, 9, 6, 2]
+
+        ArrayService svc = new ArrayService();
+        svc.sort(a1);
+        svc.swap(a2);
+
+        System.out.println(Arrays.toString(a1)); //[1, 2, 3, 4, 5, 6, 7, 8, 9]
+        System.out.println(Arrays.toString(a2)); //[2, 6, 9, 7, 4, 5, 8, 3, 1]
+    }
+}
