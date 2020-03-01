a28631a9f19e36e47360508a05107f3c9636331e
Ruslans <xmagwajx@gmail.com>
2020-03-01 20:59:51 +0200

    Lesson 7: Palindrome - add tests.

---
+++ src/test/java/lesson7/palindrome/PalindromeTest.java
@@ -0,0 +1,17 @@
+package lesson7.palindrome;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeTest {
+
+//    @Test
+//    void isPalindrome() {
+//        Palindrome palindromeTest = new Palindrome();
+//        String palindromeOneTest = "A nut for a jar of tuna.111";
+//        System.out.println(palindromeTest.isPalindrome(palindromeOneTest));
+//
+//
+//    }
+}
