7f56b049f57293c8597a668c3700c6c1174049dc
Ruslans <xmagwajx@gmail.com>
2020-03-05 20:49:10 +0200

    Lesson 7: ArrayServiceTest - added 2 tests. Check with about fillRandomly and printArray

--- src/test/java/lesson7/arrayservice/ArrayServiceTest.java
+++ src/test/java/lesson7/arrayservice/ArrayServiceTest.java
@@ -15,4 +15,33 @@ class ArrayServiceTest {
 
         assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    void sum() {
+        int [] arrayTest = ArrayService.create(3);
+
+        arrayTest[0] = 1;
+        arrayTest[1] = 2;
+        arrayTest[2] = 3;
+
+        int expectedResult = 6;
+        int actualResult = ArrayService.sum(arrayTest);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void avg() {
+        int [] arrayTest = ArrayService.create(4);
+
+        arrayTest[0] = 1;
+        arrayTest[1] = 2;
+        arrayTest[2] = 3;
+        arrayTest[3] = 4;
+
+        double expectedResult = 2.5;
+        double actualResult = ArrayService.avg(arrayTest);
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
