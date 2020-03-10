bf48550ece1d42c654df5d89db752931aa59dd42
xMAGWAJx <xmagwajx@gmail.com>
2020-03-10 14:23:09 +0200

    Lesson9: Library - in progress

--- src/main/java/lesson9/library/LibraryDemo.java
+++ src/main/java/lesson9/library/LibraryDemo.java
@@ -6,11 +6,11 @@ public class LibraryDemo {
     Book bookOne = new Book("War and Peace", "Leo Tolstoy");
     Book bookTwo = new Book("Anna Karenina", "Leo Tolstoy");
 
-//    Library library = new Library();
-//
+    Library library = new Library();
+
 //    library.addBook(bookOne);
 //    library.addBook(bookTwo);
-//
+
 //    library.findByName("War and Peace"); //Should return bookOne
 //    library.findByAuthor("Leo Tolstoy"); //Should return bookOne, bookTwo
 //
